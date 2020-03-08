2894dd888443a4fd6e8474538f4b9c254891dfc1
Blasio <gogosbalazs@gmail.com>
2020-03-08 21:07:13 +0100

    Lesson 32: HTTP Request Options

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,9 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/79bce79fc4c9b5ed7e141dd6492476b3/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/79bce79fc4c9b5ed7e141dd6492476b3/37.8267,-122.4233?units=si'
 
-request({ url: url }, (error, response) => {
-	const data = JSON.parse(response.body)
-	console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+	const temp = response.body.currently.temperature
+	const rainChance = response.body.currently.precipProbability
+	console.log(response.body.daily.data[0].summary + '\nIt is currently ' + temp + '°C out. There is a ' + rainChance + '% chance of rain.' )
 })
