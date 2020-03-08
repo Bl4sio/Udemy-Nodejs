11bcc1d802b9a470c46fe0e95a91236d5fc57e8a
Blasio <gogosbalazs@gmail.com>
2020-03-08 20:49:57 +0100

    Lesson 31: First HTTP request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,8 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout( () => {
-	console.log('2 sec timer')
-}, 2000)
+const url = 'https://api.darksky.net/forecast/79bce79fc4c9b5ed7e141dd6492476b3/37.8267,-122.4233'
 
-setTimeout( () => {
-	console.log('0 sec timer')
-}, 0)
-
-console.log('Stopping')
+request({ url: url }, (error, response) => {
+	const data = JSON.parse(response.body)
+	console.log(data.currently)
+})
