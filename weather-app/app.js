7287e272488635614a45ba5837c985af1c5ce27e
Balázs GŐGÖS <balazs.gogos@nng.com>
2020-03-09 11:48:53 +0100

    Lesson 33: Revgeo request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,19 @@
 require('dotenv').config()
 const request = require('request')
 
+
 const url = 'https://api.darksky.net/forecast/' + process.env.DARKSKY_API_KEY + '/37.8267,-122.4233?units=si'
+const geocodeURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=Budapest&key=' + process.env.GOOGLE_API_KEY + '&language:en'
+
 
 request({ url: url, json: true }, (error, response) => {
 	const temp = response.body.currently.temperature
 	const rainChance = response.body.currently.precipProbability
 	console.log(response.body.daily.data[0].summary + '\nIt is currently ' + temp + '°C out. There is a ' + rainChance + '% chance of rain.' )
+})
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+	const lat = response.body.results[0].geometry.location.lat
+	const lng = response.body.results[0].geometry.location.lng
+	console.log(lat, lng)
 })
