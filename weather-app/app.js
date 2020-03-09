f0413fe425c898445d907b1953b42905e38854b9
Balázs GŐGÖS <balazs.gogos@nng.com>
2020-03-09 15:26:09 +0100

    Lesson 37: Change forecast to callback function

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,14 @@
 require('dotenv').config()
-const request = require('request')
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-// const url = 'https://api.darksky.net/forecast/' + process.env.DARKSKY_API_KEY + '/37.8267,-122.4233?units=si'
-
-// request({ url: url, json: true }, (error, response) => {
-// 	if (error) {
-// 		console.log('Unable to connect to weather services!')
-// 	} else if (response.body.error) {
-// 		console.log('Unable to find location!')
-// 	} else {
-// 		const temp = response.body.currently.temperature
-// 		const rainChance = response.body.currently.precipProbability
-// 		console.log(response.body.daily.data[0].summary + '\nIt is currently ' + temp + '°C out. There is a ' + rainChance + '% chance of rain.' )
-// 	}
-// })
 
 geocode('Budapest, Győr', (error, data) => {
+	console.log('Error', error)
+	console.log('Data', data)
+})
+
+forecast(-75.7088, 44.1545, (error, data) => {
 	console.log('Error', error)
 	console.log('Data', data)
 })
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (lat, lng, callback) => {
+	const url = 'https://api.darksky.net/forecast/' + process.env.DARKSKY_API_KEY + '/' + lat + ',' + lng + '?units=si'
+
+	request({ url: url, json: true}, (error, response) => {
+		if (error) {
+			callback('Unable to connect to weather services!', undefined)
+		} else if (response.body.error) {
+			callback('Unable to find location!', undefined)
+		} else {
+			const temp = response.body.currently.temperature
+			const rainChance = response.body.currently.precipProbability
+			callback(undefined, response.body.daily.data[0].summary + 'It is currently ' + temp + '°C out. There is a ' + rainChance + '% chance of rain.')
+		}
+	})
+}
+
+module.exports = forecast
