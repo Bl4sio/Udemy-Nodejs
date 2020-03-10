e5d90ee0a920fb8fb01b5e8c4232a75964cbdba5
Balázs GŐGÖS <balazs.gogos@nng.com>
2020-03-10 16:18:15 +0100

    Lesson 44: HTML and JSON response

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,30 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-	res.send('Hello express!')
+	res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-	res.send('Help page')
+	res.send([{
+		name: 'Balazs',
+		age: 28
+	},
+	{
+		name: 'Evi',
+		age: 25
+	}
+	])
 })
 
 app.get('/about', (req, res) => {
-	res.send('This is about')
+	res.send('<h1>ABOUT</h1>')
 })
 
 app.get('/weather', (req, res) => {
-	res.send('Weather Information')
+	res.send({
+		forecast: 'Clear sky, 15°C',
+		location: 'Hungary, Budapest'
+	})
 })
 
 const server = app.listen(3000, () => {
