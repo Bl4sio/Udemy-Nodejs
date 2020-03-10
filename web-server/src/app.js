bb749efd85d7d23d79ec78c46ded16fcd8ec95ad
Balázs GŐGÖS <balazs.gogos@nng.com>
2020-03-10 16:31:42 +0100

    Lesson 45: Adding static html files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,11 @@
+const path = require('path')
 const express = require('express')
 
+
 const app = express()
+const publicDirPath = path.join(__dirname, "../public")
 
-app.get('', (req, res) => {
-	res.send('<h1>Weather</h1>')
-})
-
-app.get('/help', (req, res) => {
-	res.send([{
-		name: 'Balazs',
-		age: 28
-	},
-	{
-		name: 'Evi',
-		age: 25
-	}
-	])
-})
-
-app.get('/about', (req, res) => {
-	res.send('<h1>ABOUT</h1>')
-})
+app.use(express.static(publicDirPath))
 
 app.get('/weather', (req, res) => {
 	res.send({
