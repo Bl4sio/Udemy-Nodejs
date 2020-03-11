e68d67f09f36a26076b65ad88c0e88229015073d
Balázs GŐGÖS <balazs.gogos@nng.com>
2020-03-11 10:49:51 +0100

    Lesson 47: Dynamic Pages with hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,7 +5,31 @@ const express = require('express')
 const app = express()
 const publicDirPath = path.join(__dirname, "../public")
 
+// Setup hbs for express
+// They had to be in the views folder
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+	// Render a hbs view
+	res.render('index', {
+		title: 'Weather',
+		name: 'Balázs Gőgös'
+	})
+})
+
+app.get('/about', (req, res) => {
+	res.render('about',{
+		title: 'About Me',
+		name: 'Balázs Gőgös'
+	})
+})
+
+app.get('/help', (req, res) => {
+	res.render('help',{
+		text: 'You are on the Help screen. Sadly it won\'t help you, because it\'s empty'
+	})
+})
 
 app.get('/weather', (req, res) => {
 	res.send({
