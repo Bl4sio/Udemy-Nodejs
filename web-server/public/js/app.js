cb4779f843fadb3fc49324cd440c307bf3e66fdf
Blasio <gogosbalazs@gmail.com>
2020-03-12 20:24:25 +0100

    Lesson 57: HTTP request on client side

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,13 @@
 console.log('Client side js script is loaded')
+
+fetch('http://localhost:3000/weather?address=Budapest').then((response) => {
+	response.json().then((data) => {
+		if (data.error) {
+			console.log(data.error)
+			return
+		} else {
+			console.log(data.location)
+			console.log(data.forecast)
+		}
+	})
+})
