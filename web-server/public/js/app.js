f3ff891e603e49aab60a599ed89ee25a7bd74232
Blasio <gogosbalazs@gmail.com>
2020-03-12 21:28:22 +0100

    Lesson 58: Create Search Form for fetching

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,13 +1,21 @@
 console.log('Client side js script is loaded')
 
-fetch('http://localhost:3000/weather?address=Budapest').then((response) => {
-	response.json().then((data) => {
-		if (data.error) {
-			console.log(data.error)
-			return
-		} else {
-			console.log(data.location)
-			console.log(data.forecast)
-		}
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+	e.preventDefault()
+
+	const location = search.value
+
+	fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+		response.json().then((data) => {
+			if (data.error) {
+				console.log(data.error)
+			} else {
+				console.log(data.location)
+				console.log(data.forecast)
+			}
+		})
 	})
 })
