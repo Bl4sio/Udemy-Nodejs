06d733f301f26226bc1aaf4371aa97c3380400f3
Blasio <gogosbalazs@gmail.com>
2020-03-17 15:30:15 +0100

    Lesson 76: Inserting documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -8,14 +8,56 @@ const databaseName = 'task-manager'
 
 MongoClient.connect(connectionURL, { useUnifiedTopology: true, useNewUrlParser: true }, (error, client) => {
 	if (error) {
-		console.log('Unable to connect to database')
-		return
+		return console.log('Unable to connect to database')
 	}
 
 	const db = client.db(databaseName)
 
-	db.collection('users').insertOne({
-		name: 'Balazs',
-		age: 28
+	// db.collection('users').insertOne({
+	// 	name: 'Balazs',
+	// 	age: 28
+	// }, (error, result) => {
+	// 	if (error) {
+	// 		return console.log('Unable to insert user')
+	// 	}
+
+	// 	console.log(result.ops)
+	// })
+
+	// db.collection('users').insertMany([
+	// 	{
+	// 		name: 'Evi',
+	// 		age: 25
+	// 	},
+	// 	{
+	// 		name: 'Attila',
+	// 		age: 28
+	// 	}
+	// ], (error, result) => {
+	// 	if (error) {
+	// 		return console.log('Unable to insert new users!')
+	// 	}
+
+	// 	console.log(result.ops)
+	// })
+
+	db.collection('tasks').insertMany([
+		{
+			desciption: 'Shaving',
+			completed: true
+		},
+		{
+			desciption: 'Cooking dinner',
+			completed: true
+		},
+		{
+			desciption: 'Completing my CV',
+			completed: false
+		}
+	], (error, result) => {
+		if (error) {
+			return console.log('Unable to add tasks!')
+		}
+		console.log(result.ops)
 	})
 })
