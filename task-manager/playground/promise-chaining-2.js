6ba4e9638b4a5e486a4317127a97621f21b92f66
Blasio <gogosbalazs@gmail.com>
2020-03-31 17:15:30 +0200

    Lesson 96: Change promise handling to async-await

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,10 +1,19 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5e7cebf350f04042c0c9a10b').then(() => {
-	return Task.countDocuments({ completed: false })
-}).then((res) => {
-	console.log(res)
-}).catch((e) => {
-	console.log(e)
-})
+// Task.findByIdAndDelete('5e7cebf350f04042c0c9a10b').then(() => {
+// 	return Task.countDocuments({ completed: false })
+// }).then((res) => {
+// 	console.log(res)
+// }).catch((e) => {
+// 	console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+	await Task.findByIdAndDelete(id)
+	return await Task.countDocuments({ completed: false })
+}
+
+deleteTaskAndCount('5e821919603d201e946a6cd0').then((count) => {
+	console.log(count)
+}).catch(e => console.log(e))
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,13 +1,21 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-// 5e8216c7495d261f50faea9c
+// User.findByIdAndUpdate('5e7ceb3302a0043d082970ba', { age: 2 }, { new: true }).then((user) => {
+// 	console.log(user)
+// 	return User.countDocuments({ age: 2 })
+// }).then((res) => {
+// 	console.log(res)
+// }).catch((e) => {
+// 	console.log(e)
+// })
 
-User.findByIdAndUpdate('5e7ceb3302a0043d082970ba', { age: 2 }, { new: true }).then((user) => {
-	console.log(user)
-	return User.countDocuments({ age: 2 })
-}).then((res) => {
-	console.log(res)
-}).catch((e) => {
-	console.log(e)
-})
+const updateAgeAndCount = async (id, age) => {
+	const user = await User.findByIdAndUpdate(id, { age: age })
+	const count = await User.countDocuments({ age: age })
+	return count
+}
+
+updateAgeAndCount('5e7ceaf775a8472ebc1a625b', 3).then((count) => {
+	console.log(count)
+}).catch(e => console.log(e))
