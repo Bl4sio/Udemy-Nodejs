75fa326dda3d713d0d7652c61412eb30e2978629
Blasio <gogosbalazs@gmail.com>
2020-04-01 19:57:19 +0200

    Lesson 103: Learning bcryptjs

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,3 +14,18 @@ app.use(taskRouter)
 app.listen(port, () => {
 	console.log('Listening on port ' + port)
 })
+
+const bcrypt = require('bcryptjs')
+
+const myFunc = async () => {
+	const password = 'alma314$'
+	const hashedPassword = await bcrypt.hash(password, 8)
+
+	console.log(password)
+	console.log(hashedPassword)
+
+	const isMatch = await bcrypt.compare("alma314$", hashedPassword)
+	console.log(isMatch)
+}
+
+myFunc()
