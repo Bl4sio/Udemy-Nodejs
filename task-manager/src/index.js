f83c4d9fdcaac0153b9a5ba4baf4b41bf009e325
Blasio <gogosbalazs@gmail.com>
2020-04-02 15:48:09 +0200

    Lesson 108: Express middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,17 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+// 	if (req.method === 'GET') {
+// 		return res.send('GET requests are disabled')
+// 	}
+// 	next()
+// })
+
+app.use((req, res, next) => {
+	res.status(503).send('Sorry, the server is under construction!')
+})
+
 app.use(express.json())
 
 app.use(userRouter)
