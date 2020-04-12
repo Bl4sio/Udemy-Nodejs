6d1693a1abd96e3a9c935715e201fe4f42043cfe
Blasio <gogosbalazs@gmail.com>
2020-04-12 15:24:06 +0200

    Lesson 138: First test cases

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,7 +1,11 @@
-test('My 1st test case!', () => {
+const {calculateTip} = require('../src/math')
 
+test('Should calculate total with tip', () => {
+	const total = calculateTip(100, 0.2)
+	expect(total).toBe(120)
 })
 
-test('Failing test case', () => {
-	throw new Error('Failure!')
+test('Should calculate total with default tip', () => {
+	const total = calculateTip(100)
+	expect(total).toBe(115)
 })
