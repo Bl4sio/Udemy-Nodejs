ad2f094ce7bb53a1bb66605e49d7eb1a5a61433d
Blasio <gogosbalazs@gmail.com>
2020-04-13 19:15:51 +0200

    Lesson 148: Setup tests for Tasks

---
+++ task-manager/tests/fixtures/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const User = require('../../src/models/user')
+
+const userOneId = new mongoose.Types.ObjectId()
+const userOne = {
+	_id: userOneId,
+	name: 'Béla',
+	email: 'bela@test.com',
+	password: 'belapass123',
+	tokens: [{
+		token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
+	}]
+}
+
+const setupDatabase = async () => {
+	await User.deleteMany()
+	await new User(userOne).save()
+}
+
+module.exports = {
+	userOneId, userOne, setupDatabase
+}
