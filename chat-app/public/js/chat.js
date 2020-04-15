0efea952b7d5c4af12a6602d752a5bc502c584d6
Blasio <gogosbalazs@gmail.com>
2020-04-15 21:45:20 +0200

    Lesson 156: Send/Recieve messages globally

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,9 +1,13 @@
 const socket = io()
 
-socket.on('countUpdated', (count) => {
-	console.log('Count has been updated', count)
+socket.on('message', (msg) => {
+	console.log(msg)
 })
 
-document.querySelector('#increment').addEventListener('click', () => {
-	socket.emit('increment')
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+	e.preventDefault()
+
+	const message = e.target.elements.message.value
+
+	socket.emit('sendMessage', message)
 })
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,16 +12,13 @@ const publicDirPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirPath))
 
-let count = 0
-
 io.on('connection', (socket) => {
 	console.log('New websocket connection!')
 
-	socket.emit('countUpdated', count)
-	
-	socket.on('increment', () => {
-		count++
-		io.emit('countUpdated', count)
+	socket.emit('message', 'Welcome on my Chat-app!')
+
+	socket.on('sendMessage', (msg) => {
+		io.emit('message', msg)
 	})
 })
 
